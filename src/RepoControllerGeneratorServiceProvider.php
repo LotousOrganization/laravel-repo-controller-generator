a354176358533f74a428fae420860e4cbe555791
Sobhan Aali <95542970+Sobhanaali@users.noreply.github.com>
2025-06-11 13:19:45 +0330

    Update RepoControllerGeneratorServiceProvider.php

--- src/RepoControllerGeneratorServiceProvider.php
+++ src/RepoControllerGeneratorServiceProvider.php
@@ -3,7 +3,7 @@
 namespace SobhanAali\LaravelRepoControllerGenerator;
 
 use Illuminate\Support\ServiceProvider;
-use SobhanAali\RepoControllerGenerator\Console\Commands\MakeControllerRepoCommand;
+use SobhanAali\LaravelRepoControllerGenerator\Console\Commands\MakeControllerRepoCommand;
 
 class RepoControllerGeneratorServiceProvider extends ServiceProvider
 {
