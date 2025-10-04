73650362b10c47ac2d11ea13ee002024952cac53
sobhanaali <sobhanaali80@gmail.com>
2025-10-04 10:23:09 +0330

    fix

--- src/BaseClass/BaseRepositoryInterface.php
+++ src/BaseClass/BaseRepositoryInterface.php
@@ -2,16 +2,13 @@
 
 namespace SobhanAali\LaravelRepoControllerGenerator\BaseClass;
 
-use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Database\Eloquent\Model;
-
 interface BaseRepositoryInterface
 {
-    public function all(FormRequest $request);
+    public function all($request);
 
-    public function create(FormRequest $request);
+    public function create($request);
 
-    public function update(FormRequest $request ,  Model $object);
+    public function update($request ,  $object);
 
-    public function delete(Model $object);
+    public function delete($object);
 }
